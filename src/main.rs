a33e7f4d2f10e9302fef1c93d4066057b4dfd0f1
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-21 22:52:43 +0000

    feat: receive user input and store it in a variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::io::stdin;
+
 fn main() {
     println!("===================");
     println!("GUESSING THE NUMBER");
     println!("===================");
+
+    let mut user_input = String::new();
+
+    println!("Please input your guess: ");
+
+    stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read the line");
 }
