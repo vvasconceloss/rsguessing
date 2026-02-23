d8551199c16bdf50af9f2576feb35c76e302642d
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-23 13:28:29 +0000

    feat: generate a random value with rand and compare it with the user's input

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("===================");
 
     let mut user_input = String::new();
+    let secret_number: u32 = rand::random_range(1..=5);
 
     println!("Please input your guess: ");
 
@@ -19,7 +20,15 @@ fn main() {
         .expect("Failed to convert to u32");
 
     match user_guess {
-        1..=5 => {}
+        1..=5 => {
+            if user_guess != secret_number {
+                println!("You lose... The number was: {}", secret_number);
+            }
+
+            if user_guess == secret_number {
+                println!("You win! Congratulations!");
+            }
+        }
         _ => println!("The number must be between 1 and 5"),
     }
 }
